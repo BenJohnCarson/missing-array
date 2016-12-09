40e39a5170ad4738f8b141f331e88b0ed0de575e
Benjamin Carson <benjohncarson@gmail.com>
2016-12-09 15:29:39 +0000

    Passes all tests without errors

--- missing-array.rb
+++ missing-array.rb
@@ -1,15 +1,13 @@
 def getLengthOfMissingArray(array_of_arrays)
-    if (array_of_arrays == nil) || !array_of_arrays.empty?
+    if !(array_of_arrays == nil) && !array_of_arrays.empty?
+        array_of_arrays.each { |array| return 0 if (array == nil) || (array.empty?)}
+        
         sorted = array_of_arrays.sort_by(&:length)
         num = sorted[0].length
 
         sorted.each do |array|
-            if !array.empty?
-                return (array.length - 1) if (array.length != num)
-                num += 1
-            else
-            0
-            end
+            return (array.length - 1) if (array.length != num)
+            num += 1
         end
     else
         0
